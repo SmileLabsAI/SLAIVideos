76b015d0895e9fd1b33f99c547e454868555c952
Smile Labs AI <smilelabsai@gmail.com>
2025-02-04 19:16:25 -0300

    Criaçao da home

--- script.js
+++ script.js
@@ -153,3 +153,17 @@ document.addEventListener("DOMContentLoaded", function () {
         mostrarPlano(index);
     });
 });
+function togglePassword() {
+    const passwordInput = document.getElementById("password");
+    const toggleIcon = document.querySelector(".toggle-password");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleIcon.classList.remove("fa-eye");
+        toggleIcon.classList.add("fa-eye-slash"); // Ícone de "olho fechado"
+    } else {
+        passwordInput.type = "password";
+        toggleIcon.classList.remove("fa-eye-slash");
+        toggleIcon.classList.add("fa-eye"); // Ícone de "olho aberto"
+    }
+}
